990c6bd9c829542fa9d533995e6621c347dfc7fb
jmabuc <jmabuc@5465a33e-6632-11de-a7d7-65642d686af2>
2009-08-26 17:59:45 +0000

    First version.
    It currently uses F6 for single-click and F7 for double-click. Both keys are hardcoded. Messages shown are also hardcoded, and only in English.

---
+++ windows/src/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by double-clicker3.rc
+//
+#define IDC_MYICON                      2
+#define IDD_DOUBLECLICKER3_DIALOG       102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_DOUBLECLICKER3              107
+#define IDI_SMALL                       108
+#define IDC_DOUBLECLICKER3              109
+#define IDR_MAINFRAME                   128
+#define IDI_AUTELIC                     129
+#define IDR_MENU1                       130
+#define IDR_AUTELIC                     130
+#define ID_CONTEXT_EXIT                 32771
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
