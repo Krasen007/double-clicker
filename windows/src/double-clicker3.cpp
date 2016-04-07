02c6465da5bfa88d1b70062995f8b8191165b855
Krasen Ivanov <krasen007@gmail.com>
2016-04-07 21:08:55 +0300

    Code cleanup and fixes
    Fixed 4 warnings and did some code cleanup

--- windows/src/double-clicker3.cpp
+++ windows/src/double-clicker3.cpp
@@ -1,6 +1,6 @@
 /*
-  <double-clicker3.cpp> - <Simulates mouse single-click and double-click by using keyboard hotkeys>
-  
+<double-clicker3.cpp> - <Simulates mouse single-click and double-click by using keyboard hotkeys>
+
 Copyright 2008-2009 Autelic Association (http://www.autelic.org)
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,9 +8,9 @@ you may not use this file except in compliance with the License.
 
 You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software distributed 
-under the License is distributed on an "AS IS" BASIS,WITHOUT WARRANTIES OR CONDITIONS 
-OF ANY KIND, either express or implied. See the License for the specific language 
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS,WITHOUT WARRANTIES OR CONDITIONS
+OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
 
@@ -32,14 +32,17 @@ HINSTANCE hInst;								// current instance
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
+//TODO: Fix key selection
+int selectSingleClickKey = VK_F9;
+int selectDoubleClickKey = VK_F10;
 
-const int singleClickVKey = VK_F6;
-const int doubleClickVKey = VK_F7;
+const int singleClickVKey = selectSingleClickKey; //VK_F9;
+const int doubleClickVKey = selectDoubleClickKey; //VK_F10;
 
 static INPUT g_mouseLeftClick[2];
 static INPUT g_mouseDoubleLeftClick[4];
 
-static TCHAR g_prgInfoMsg[1024] = _T("Autelic\n");
+static TCHAR g_prgInfoMsg[1024] = _T("Double Clicker\n");
 
 // Forward declarations of functions included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
@@ -49,18 +52,18 @@ INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 static BOOL DCInit()
 {
-	g_mouseLeftClick[0].type = INPUT_MOUSE; 
+	g_mouseLeftClick[0].type = INPUT_MOUSE;
 	g_mouseLeftClick[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-	g_mouseLeftClick[1].type = INPUT_MOUSE; 
+	g_mouseLeftClick[1].type = INPUT_MOUSE;
 	g_mouseLeftClick[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
 
-	g_mouseDoubleLeftClick[0].type = INPUT_MOUSE; 
+	g_mouseDoubleLeftClick[0].type = INPUT_MOUSE;
 	g_mouseDoubleLeftClick[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-	g_mouseDoubleLeftClick[1].type = INPUT_MOUSE; 
+	g_mouseDoubleLeftClick[1].type = INPUT_MOUSE;
 	g_mouseDoubleLeftClick[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
-	g_mouseDoubleLeftClick[2].type = INPUT_MOUSE; 
+	g_mouseDoubleLeftClick[2].type = INPUT_MOUSE;
 	g_mouseDoubleLeftClick[2].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-	g_mouseDoubleLeftClick[3].type = INPUT_MOUSE; 
+	g_mouseDoubleLeftClick[3].type = INPUT_MOUSE;
 	g_mouseDoubleLeftClick[3].mi.dwFlags = MOUSEEVENTF_LEFTUP;
 
 	return TRUE;
@@ -72,41 +75,41 @@ static BOOL RegisterHotKeys()
 
 	// register single-click hotkey
 	//
-    if (RegisterHotKey(
-        NULL,
-        0,	//hot key identifier
-        0, //MOD_SHIFT, //MOD_ALT,// | MOD_NOREPEAT,
-        singleClickVKey))
-    {
-        _stprintf_s(buf, 1024, _T("F%d -> single-click\n"), singleClickVKey - VK_F1 + 1 );
-		OutputDebugString( buf );
-		_tcscat_s( g_prgInfoMsg, _countof(g_prgInfoMsg), buf );
-    }
+	if (RegisterHotKey(
+		NULL,
+		0,	//hot key identifier
+		0, //MOD_SHIFT, //MOD_ALT,// | MOD_NOREPEAT,
+		singleClickVKey))
+	{
+		_stprintf_s(buf, 1024, _T("F%d -> single-click\n"), singleClickVKey - VK_F1 + 1);
+		OutputDebugString(buf);
+		_tcscat_s(g_prgInfoMsg, _countof(g_prgInfoMsg), buf);
+	}
 	else
-    {
-        _stprintf_s(buf, 1024, _T("Couldn't assign F% key for single-click"), singleClickVKey - VK_F1 + 1 );
-		OutputDebugString( buf );
-		_tcscat_s( g_prgInfoMsg, _countof(g_prgInfoMsg), buf );
-    }
+	{
+		_stprintf_s(buf, 1024, _T("Couldn't assign F%d key for single-click"), singleClickVKey - VK_F1 + 1);
+		OutputDebugString(buf);
+		_tcscat_s(g_prgInfoMsg, _countof(g_prgInfoMsg), buf);
+	}
 
 	// register double-click hotkey
 	//
- 	if (RegisterHotKey(
-        NULL,
-        1,
-        0, //MOD_SHIFT, //MOD_ALT,// | MOD_NOREPEAT,
-        doubleClickVKey))
-    {
-        _stprintf_s(buf,1024, _T("F%d -> double-click\n"), doubleClickVKey - VK_F1 + 1 );
-		OutputDebugString( buf );
-		_tcscat_s( g_prgInfoMsg, _countof(g_prgInfoMsg), buf );
-    }
+	if (RegisterHotKey(
+		NULL,
+		1,
+		0, //MOD_SHIFT, //MOD_ALT,// | MOD_NOREPEAT,
+		doubleClickVKey))
+	{
+		_stprintf_s(buf, 1024, _T("F%d -> double-click\n"), doubleClickVKey - VK_F1 + 1);
+		OutputDebugString(buf);
+		_tcscat_s(g_prgInfoMsg, _countof(g_prgInfoMsg), buf);
+	}
 	else
 	{
-        _stprintf_s(buf, 1024, _T("Couldn't assign F% key for double-click"), doubleClickVKey - VK_F1 + 1 );
-		OutputDebugString( buf );
-		_tcscat_s( g_prgInfoMsg, _countof(g_prgInfoMsg), buf );
-    }
+		_stprintf_s(buf, 1024, _T("Couldn't assign F%d key for double-click"), doubleClickVKey - VK_F1 + 1);
+		OutputDebugString(buf);
+		_tcscat_s(g_prgInfoMsg, _countof(g_prgInfoMsg), buf);
+	}
 
 	return TRUE;
 }
@@ -117,52 +120,51 @@ static BOOL NotificationIconMessage(HWND hWnd, DWORD dwMessage)
 	nid.cbSize = sizeof(NOTIFYICONDATA);
 	nid.hWnd = hWnd;
 	nid.uID = IDI_AUTELIC;
-	nid.uFlags = NIF_MESSAGE|NIF_ICON;
+	nid.uFlags = NIF_MESSAGE | NIF_ICON;
 	nid.uCallbackMessage = MYTRAY_ICON_MSG;
-	nid.uFlags = NIF_MESSAGE|NIF_ICON|NIF_TIP;
-	nid.hIcon = LoadIcon(hInst, MAKEINTRESOURCE (IDI_AUTELIC));
-	_tcscpy_s( nid.szTip, _countof( nid.szTip ), (LPCTSTR)g_prgInfoMsg );
+	nid.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
+	nid.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_AUTELIC));
+	_tcscpy_s(nid.szTip, _countof(nid.szTip), (LPCTSTR)g_prgInfoMsg);
 
 	return Shell_NotifyIcon(dwMessage, &nid);
 }
 
-static BOOL ShowContextMenu( HWND hWnd )
+static BOOL ShowContextMenu(HWND hWnd)
 {
-	HMENU ctxMenu = LoadMenu( hInst, MAKEINTRESOURCE (IDR_AUTELIC) );
-	if( NULL == ctxMenu )
+	HMENU ctxMenu = LoadMenu(hInst, MAKEINTRESOURCE(IDR_AUTELIC));
+	if (NULL == ctxMenu)
 	{
-		OutputDebugString( _T("Can´t retrieve app main menu") );
+		OutputDebugString(_T("Can´t retrieve app main menu"));
 		return FALSE;
 	}
 
-	HMENU trackPopup = GetSubMenu(ctxMenu, 0 );
+	HMENU trackPopup = GetSubMenu(ctxMenu, 0);
 
 	// Get the position of the cursor
 	POINT pCursor;
 	GetCursorPos(&pCursor);
 
 	// needed
-    SetForegroundWindow(hWnd);
+	SetForegroundWindow(hWnd);
 
 	// Popup the menu with cursor position as the coordinates to pop it up
 	TrackPopupMenu(trackPopup, TPM_LEFTBUTTON | TPM_RIGHTALIGN, pCursor.x, pCursor.y, 0, hWnd, NULL);
 
-	DestroyMenu( ctxMenu );
+	DestroyMenu(ctxMenu);
 
 	return TRUE;
 }
 
-
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nCmdShow);
 
- 	// TODO: Place code here.
+	// TODO: Place code here.
 	MSG msg;
 	HACCEL hAccelTable;
 
@@ -173,50 +175,49 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
 	// Perform application initialization:
 //	if (!InitInstance (hInstance, nCmdShow))
-	if (!InitInstance (hInstance, SW_HIDE))
+	if (!InitInstance(hInstance, SW_HIDE))
 	{
 		return FALSE;
 	}
 
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DOUBLECLICKER3));
 
-
 	// Main message loop:
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
-		if( msg.message == WM_HOTKEY )
-        {
-			#ifdef _DEBUG
-            OutputDebugString(_T("WM_HOTKEY received\n"));
-			#endif
+		if (msg.message == WM_HOTKEY)
+		{
+#ifdef _DEBUG
+			OutputDebugString(_T("WM_HOTKEY received\n"));
+#endif
 
 			int vKey = (msg.lParam) >> 16;
 			UINT res = 0;
 
-			assert( singleClickVKey == vKey || doubleClickVKey == vKey );
-			if( singleClickVKey == vKey )
+			assert(singleClickVKey == vKey || doubleClickVKey == vKey);
+			if (singleClickVKey == vKey)
 			{
-				res = SendInput(      
-					sizeof( g_mouseLeftClick ) / sizeof( g_mouseLeftClick[0] ),
+				res = SendInput(
+					sizeof(g_mouseLeftClick) / sizeof(g_mouseLeftClick[0]),
 					&g_mouseLeftClick[0],
-					sizeof( INPUT )
+					sizeof(INPUT)
 					);
-				assert( 2 == res );
+				assert(2 == res);
 			}
-			else if( doubleClickVKey == vKey )
+			else if (doubleClickVKey == vKey)
 			{
-				res = SendInput(      
-					sizeof( g_mouseDoubleLeftClick ) / sizeof( g_mouseDoubleLeftClick[0] ),
+				res = SendInput(
+					sizeof(g_mouseDoubleLeftClick) / sizeof(g_mouseDoubleLeftClick[0]),
 					&g_mouseDoubleLeftClick[0],
-					sizeof( INPUT )
+					sizeof(INPUT)
 					);
-				assert( 4 == res );
+				assert(4 == res);
 			}
 			else
 			{
-				OutputDebugString( _T("Unknown hotkey received\n") );
+				OutputDebugString(_T("Unknown hotkey received\n"));
 			}
-        }
+		}
 		else
 		{
 			if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
@@ -227,10 +228,8 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 		}
 	}
 
-	return (int) msg.wParam;
+	return (int)msg.wParam;
 }
-
-
 
 //
 //  FUNCTION: MyRegisterClass()
@@ -251,17 +250,17 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
 	wcex.cbSize = sizeof(WNDCLASSEX);
 
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DOUBLECLICKER3));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_DOUBLECLICKER3);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DOUBLECLICKER3));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCE(IDC_DOUBLECLICKER3);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
 	return RegisterClassEx(&wcex);
 }
@@ -283,11 +282,11 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	hInst = hInstance; // Store instance handle in our global variable
 
 	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-	  CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
 
 	if (!hWnd)
 	{
-	  return FALSE;
+		return FALSE;
 	}
 
 	ShowWindow(hWnd, nCmdShow);
@@ -326,7 +325,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
+		wmId = LOWORD(wParam);
 		wmEvent = HIWORD(wParam);
 		// Parse the menu selections:
 		switch (wmId)
@@ -355,33 +354,32 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 
 	case MYTRAY_ICON_MSG:
+	{
+		switch (lParam)
 		{
-			switch (lParam)
+		case WM_LBUTTONDBLCLK:
+			switch (wParam)
 			{
-				case WM_LBUTTONDBLCLK:
-					switch (wParam)
-					{
-						case IDI_AUTELIC:
-							//ShowWindow(hWnd, SW_NORMAL);
-							//SetForegroundWindow(hWnd);
-							//SetFocus(hWnd);
-							//return TRUE;
-							break;
-						default:
-							break;
-					}
-					break;
-				case WM_RBUTTONDOWN:
-				case WM_CONTEXTMENU:
-					ShowContextMenu(hWnd);
-					break;
-
-				default:
-					break;
+			case IDI_AUTELIC:
+				//ShowWindow(hWnd, SW_NORMAL);
+				//SetForegroundWindow(hWnd);
+				//SetFocus(hWnd);
+				//return TRUE;
+				break;
+			default:
+				break;
 			}
-		}
-		break;
+			break;
+		case WM_RBUTTONDOWN:
+		case WM_CONTEXTMENU:
+			ShowContextMenu(hWnd);
+			break;
 
+		default:
+			break;
+		}
+	}
+	break;
 
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
